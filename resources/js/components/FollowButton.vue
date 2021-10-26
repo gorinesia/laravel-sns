e5675cd3a50d257c859faf6b65dce2a812784597
Akinori Sakakibara <gorion.1213@gmail.com>
2021-10-27 07:12:31 +0900

    Create follow table and set the follow method

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -3,6 +3,7 @@
     <button
       class="btn-sm shadow-none border border-primary p-2"
       :class="buttonColor"
+      @click="clickFollow"
     >
       <i class="mr-1" :class="buttonIcon"></i>
       {{ buttonText }}
@@ -14,8 +15,15 @@
 export default {
   props: {
     initiallsFollowedBy: {
-      type: Button,
+      type: Boolean,
       default: false,
+    },
+    authorized: {
+      type: Boolean,
+      default: false,
+    },
+    endpoint: {
+      type: String,
     },
   },
   data() {
@@ -32,6 +40,26 @@ export default {
     },
     buttonText() {
       return this.isFollowedBy ? "フォロー中" : "フォロー";
+    },
+  },
+  methods: {
+    clickFollow() {
+      if (!this.authorized) {
+        alert("フォロー機能はログイン中のみ使用できます");
+        return;
+      }
+
+      this.isFollowedBy ? this.unfolllow() : this.follow();
+    },
+    async follow() {
+      const response = await axios.put(this.endpoint);
+
+      this.isFollowedBy = true;
+    },
+    async unfollow() {
+      const response = await axios.delete(this.endpoint);
+
+      this.isFollowedBy = false;
     },
   },
 };
