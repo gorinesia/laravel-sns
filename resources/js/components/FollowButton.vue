c69f6804ab5b8d3f2cd1b954c198a6fba71d1996
Akinori Sakakibara <gorion.1213@gmail.com>
2021-10-26 22:29:27 +0900

    Set the follow and unfollow action method

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -4,38 +4,35 @@
       class="btn-sm shadow-none border border-primary p-2"
       :class="buttonColor"
     >
-      <i
-        class="mr-1"
-        :class="buttonIcon"
-      ></i>
+      <i class="mr-1" :class="buttonIcon"></i>
       {{ buttonText }}
     </button>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        isFollowedBy: false,
-      }
+export default {
+  props: {
+    initiallsFollowedBy: {
+      type: Button,
+      default: false,
     },
-    computed: {
-      buttonColor() {
-        return this.isFollowedBy
-          ? 'bg-primary text-white'
-          : 'bg-white'
-      },
-      buttonIcon() {
-        return this.isFollowedBy
-          ? 'fas fa-user-check'
-          : 'fas fa-user-plus'
-      },
-      buttonText() {
-        return this.isFollowedBy
-          ? 'フォロー中'
-          : 'フォロー'
-      },
+  },
+  data() {
+    return {
+      isFollowedBy: this.initiallsFollowedBy,
+    };
+  },
+  computed: {
+    buttonColor() {
+      return this.isFollowedBy ? "bg-primary text-white" : "bg-white";
     },
-  }
+    buttonIcon() {
+      return this.isFollowedBy ? "fas fa-user-check" : "fas fa-user-plus";
+    },
+    buttonText() {
+      return this.isFollowedBy ? "フォロー中" : "フォロー";
+    },
+  },
+};
 </script>
