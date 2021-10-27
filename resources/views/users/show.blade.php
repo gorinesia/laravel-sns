ed5125208f0fac6a85f69543b1ff9dcf07e7ec39
Akinori Sakakibara <gorion.1213@gmail.com>
2021-10-27 13:34:59 +0900

    Count the follow number

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -25,10 +25,10 @@
       <div class="card-body">
         <div class="card-text">
           <a href="" class="text-muted">
-            10 フォロー
-          </a>
-          <a href="" class="text-muted">
-            10 フォロワー
+            {{ $user->count_followings }} フォロワー
+        </a>
+        <a href="" class="text-muted">
+            {{ $user->count_followers }} フォロワー
           </a>
         </div>
       </div>
